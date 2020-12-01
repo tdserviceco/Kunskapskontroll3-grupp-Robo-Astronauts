fb2e3fd4b1c9079fda2d820fe0fb4edc5379d7a8
Sundholmare <70331013+Sundholmare@users.noreply.github.com>
2020-12-01 11:14:02 +0100

    main.js start

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const url = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=61a0027b6159658475368cfa3414d739&tags=animal&media=photos&per_page=12&page=1&format=json&nojsoncallback=1';
+
+let images;
+
+fetch(url).then(function(response){
+    return response.json();
+}).then(function(data){
+    images = data.photos.photo;
+})
