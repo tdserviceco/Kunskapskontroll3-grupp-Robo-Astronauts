27783987710a2eee8aaa2dac95e8873f2b557d8d
Tommy Danielsson <tdserviceco@gmail.com>
2020-12-07 13:32:16 +0100

    Adderade shuffle och fixade lite kod struktur

--- js/functions.js
+++ js/functions.js
@@ -1,50 +1,76 @@
-function Game(apiKey) {
-  const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=nature&media=photos&per_page=12&page=1&format=json&nojsoncallback=1`;
-  let images = [[], []];
+let matches = 0;
 
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
+function Game(apiKey) {
+  let images = [[], []];
+  getData(apiKey).then(data => {
       for (let i = 0; i < 12; i++) {
-        images[0][i] = data.photos.photo[i];
-        images[1][i] = data.photos.photo[i];
+          images[0][i] = data.photos.photo[i];
+          images[1][i] = data.photos.photo[i];
       }
       imageGeneration(images);
-    });
+  })
 
   //Från Prototype
-  this.match(".card");
+  this.match('.card')
+}
+
+async function getData(apiKey) {
+  const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=dogs&tags=dogs&media=photos&per_page=12&page=1&format=json&nojsoncallback=1`;
+  let response = await fetch(url);
+  return response.json()
 }
 
 function imageGeneration(images) {
-  // återställer score vid laddning av kort
-  document.querySelector(".score-board").textContent = 0 + " Clicks";
+  let allCards = document.querySelectorAll('.card');
+    // Lägger till IMG element tagg till .card klassen.
 
-  let allCards = document.querySelectorAll(".card");
-  // Lägger till IMG element tagg till .card klassen.
-  for (let i = 0; i < 24; i++) {
-    let img = document.createElement("img");
-    allCards[i].appendChild(img);
-    img.classList.add("hide");
-  }
-
-  let allImg = document.querySelectorAll("img");
-
-  // Loopa igenom 2ggr och sedan spotta ut 24 bilder pga vi hämtar 12st och sparar ner de i en array
-  for (let i = 0; i < 2; i++) {
-    for (let j = 0; j < 12; j++) {
-      let id = images[i][j].id;
-      let secret = images[i][j].secret;
-      let server = images[i][j].server;
-      let size = "q";
-
-      let result = `https://live.staticflickr.com/${server}/${id}_${secret}_${size}.jpg`;
-
-      allImg[j].src = result;
-      allImg[j + 12].src = result;
+    for (let i = 0; i < 24; i++) {
+        let img = document.createElement('img');
+        allCards[i].appendChild(img);
+        img.classList.add('hide')
     }
+
+    let result;
+    let convertNodeListToArray = [];
+    // Loopa igenom 2ggr och sedan spotta ut 24 bilder pga vi hämtar 12st och sparar ner de i en array
+    for (let i = 0; i < 2; i++) {
+        for (let j = 0; j < 12; j++) {
+            let id = images[i][j].id;
+            let secret = images[i][j].secret;
+            let server = images[i][j].server;
+            let size = 'q';
+            result = `https://live.staticflickr.com/${server}/${id}_${secret}_${size}.jpg`;
+            convertNodeListToArray.push(result);
+        }
+
+    }
+    shuffle(convertNodeListToArray)
+}
+
+
+/**
+ * Blandar array index i olika ordning.
+ * @param Ideé från https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+ * 
+ */
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  return displayImage(a);
+}
+
+
+function displayImage(images) {
+  let img = document.querySelectorAll('.card img');
+  // console.log(images)
+  for (let i = 0; i < images.length; i++) {
+      img[i].src = images[i];
   }
 }
 
@@ -75,7 +101,7 @@ function disableMatch(card) {
   });
 }
 
-let matches = 0;
+
 // Om matching är True, öka matches, annars vänd tillbaka korten efter 1.5 sekunder. När matches når 12, alerta och ladda om
 function gamePlay(img, clicks) {
   if (isItMatching(img)) {
